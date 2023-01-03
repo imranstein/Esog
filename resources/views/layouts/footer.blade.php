bf5a05a47e4fcd8541542508c134447245d0374c
imranstein <imranabdul22@gmail.com>
2023-01-03 14:49:10 +0300

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -22,40 +22,40 @@
           </div>
       </div>
   </footer> --}}
-   <footer class="footer">
-       <div class="container-fluid">
-           <nav class="float-left">
-               <ul>
-                   <li>
-                       <a href="https://www.creative-tim.com">
-                           Creative Tim
-                       </a>
-                   </li>
-                   <li>
-                       <a href="https://creative-tim.com/presentation">
-                           About Us
-                       </a>
-                   </li>
-                   <li>
-                       <a href="http://blog.creative-tim.com">
-                           Blog
-                       </a>
-                   </li>
-                   <li>
-                       <a href="https://www.creative-tim.com/license">
-                           Licenses
-                       </a>
-                   </li>
-               </ul>
-           </nav>
-           <div class="copyright float-right">
-               &copy;
-               <script>
-                   document.write(new Date().getFullYear())
+<footer class="footer">
+    <div class="container-fluid">
+        {{-- <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="https://www.creative-tim.com">
+                        Creative Tim
+                    </a>
+                </li>
+                <li>
+                    <a href="https://creative-tim.com/presentation">
+                        About Us
+                    </a>
+                </li>
+                <li>
+                    <a href="http://blog.creative-tim.com">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.creative-tim.com/license">
+                        Licenses
+                    </a>
+                </li>
+            </ul>
+        </nav> --}}
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
 
-               </script>, made with <i class="material-icons">favorite</i> by
-               <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
-           </div>
-       </div>
-   </footer>
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.greentechet.com" target="_blank">GreenTech</a> for a better web.
 
+        </div>
+    </div>
+</footer>
