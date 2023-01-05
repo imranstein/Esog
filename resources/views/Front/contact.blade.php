91d4c8e17e44af9535d077985bfbecc2b886e6e2
imranstein <imranabdul22@gmail.com>
2023-01-05 11:11:46 +0300

    Update contact.blade.php

--- resources/views/Front/contact.blade.php
+++ resources/views/Front/contact.blade.php
@@ -18,7 +18,75 @@
 
 
 <!-- Contact Start -->
-<div class="container-fluid py-5">
+<div class="map-area">
+    {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22236.40558254599!2d-118.25292394686001!3d34.057682914027104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2z4Kay4Ka4IOCmj-CmnuCnjeCmnOCnh-CmsuCnh-CmuCwg4KaV4KeN4Kav4Ka-4Kay4Ka_4Kar4KeL4Kaw4KeN4Kao4Ka_4Kav4Ka84Ka-LCDgpq7gpr7gprDgp43gppXgpr_gpqgg4Kav4KeB4KaV4KeN4Kak4Kaw4Ka-4Ka34KeN4Kaf4KeN4Kaw!5e0!3m2!1sbn!2sbd!4v1532328708137" allowfullscreen></iframe> --}}
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5468211436487!2d38.75561691459064!3d9.013779093531442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85bf62b2b90b%3A0xad11c2826d8c0b3c!2sGhion%20Pastry!5e0!3m2!1sen!2set!4v1672906247987!5m2!1sen!2set" allowfullscreen></iframe>
+
+</div>
+<!-- ##### Google Maps End ##### -->
+
+<!-- ##### Contact Area Start ##### -->
+<section class="contact-area section-padding-100-0">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 col-lg-6">
+                <div class="contact-content mb-100">
+                    <h4>Get In Touch</h4>
+                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia dese mollit anim id est laborum, consectetur adipisicing elit.</p>
+                    <!-- Single Contact Info -->
+                    <div class="single-contact-info">
+                        <h6>Location:</h6>
+                        <h4>Tsehafi Tízaz Teferawork Keda Building, Ras Desta Damtew Avenue</h4>
+                    </div>
+
+                    <!-- Single Contact Info -->
+                    <div class="single-contact-info">
+                        <h6>Email:</h6>
+                        {{-- prompt to email --}}
+                        <h4><a href="mailto: info@esog-eth.org"> info@esog-eth.org</a></h4>
+                    </div>
+
+                    <!-- Single Contact Info -->
+                    <div class="single-contact-info">
+                        <h6>Phone:</h6>
+                        {{-- prompt to call --}}
+                        <h4><a href="tel:+251115506068">+251115506068</a></h4>
+                        <h4></h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-6">
+                <div class="contact-content mb-100">
+                    <h4>Contact Form</h4>
+
+                    <!-- Contact Form Area -->
+                    <div class="contact-form-area">
+                        <form method="POST" action="{{ route('front.contact.send') }}">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" id="contact-name" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" id="contact-email" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="contact-subject" placeholder="Subject">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                            </div>
+                            <button type="submit" class="btn nikki-btn mt-15">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+{{-- <div class="container-fluid py-5">
     <div class="container py-5">
         <div class="text-center mx-auto mb-5" style="max-width: 500px;">
             <h4 class="display-6">Please Feel Free To Contact Us</h1>
@@ -61,29 +129,29 @@
             <div class="col-lg-8">
                 <div class="bg-primary p-5 m-5 mb-0">
                     <form method="POST" action="{{ route('front.contact.send') }}">
-                        @csrf
-                        <div class="row g-3">
-                            <div class="col-12 col-sm-6">
-                                <input type="text" class="form-control bg-light border-0" placeholder="Your Name" style="height: 55px;" name="name">
-                            </div>
-                            <div class="col-12 col-sm-6">
-                                <input type="email" class="form-control bg-light border-0" placeholder="Your Email" style="height: 55px;" name="email">
-                            </div>
-                            <div class="col-12">
-                                <input type="text" class="form-control bg-light border-0" placeholder="Subject" style="height: 55px;" name="subject">
-                            </div>
-                            <div class="col-12">
-                                <textarea class="form-control bg-light border-0" rows="5" placeholder="Message" name="message"></textarea>
-                            </div>
-                            <div class="col-12">
-                                <button class="btn btn-secondary w-100 py-3" type="submit">Send Message</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+@csrf
+<div class="row g-3">
+    <div class="col-12 col-sm-6">
+        <input type="text" class="form-control bg-light border-0" placeholder="Your Name" style="height: 55px;" name="name">
+    </div>
+    <div class="col-12 col-sm-6">
+        <input type="email" class="form-control bg-light border-0" placeholder="Your Email" style="height: 55px;" name="email">
+    </div>
+    <div class="col-12">
+        <input type="text" class="form-control bg-light border-0" placeholder="Subject" style="height: 55px;" name="subject">
+    </div>
+    <div class="col-12">
+        <textarea class="form-control bg-light border-0" rows="5" placeholder="Message" name="message"></textarea>
+    </div>
+    <div class="col-12">
+        <button class="btn btn-secondary w-100 py-3" type="submit">Send Message</button>
     </div>
 </div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div> --}}
 
 @endsection
