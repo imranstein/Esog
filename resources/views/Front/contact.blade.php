edb0b0f87dcc938cc18491ecd6eeb149cc4875d1
imranstein <imranabdul22@gmail.com>
2023-01-05 11:13:12 +0300

    Update contact.blade.php

--- resources/views/Front/contact.blade.php
+++ resources/views/Front/contact.blade.php
@@ -20,7 +20,7 @@
 <!-- Contact Start -->
 <div class="map-area">
     {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22236.40558254599!2d-118.25292394686001!3d34.057682914027104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2z4Kay4Ka4IOCmj-CmnuCnjeCmnOCnh-CmsuCnh-CmuCwg4KaV4KeN4Kav4Ka-4Kay4Ka_4Kar4KeL4Kaw4KeN4Kao4Ka_4Kav4Ka84Ka-LCDgpq7gpr7gprDgp43gppXgpr_gpqgg4Kav4KeB4KaV4KeN4Kak4Kaw4Ka-4Ka34KeN4Kaf4KeN4Kaw!5e0!3m2!1sbn!2sbd!4v1532328708137" allowfullscreen></iframe> --}}
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5468211436487!2d38.75561691459064!3d9.013779093531442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85bf62b2b90b%3A0xad11c2826d8c0b3c!2sGhion%20Pastry!5e0!3m2!1sen!2set!4v1672906247987!5m2!1sen!2set" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5476579315255!2d38.75573901459058!3d9.01370239353145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b856ccc941901%3A0x5053eb99e57c4eb0!2sESOG!5e0!3m2!1sen!2set!4v1672906357801!5m2!1sen!2set" allowfullscreen></iframe>
 
 </div>
 <!-- ##### Google Maps End ##### -->
