7f5106bcd13d3d786628117e43d8a5d3e986ad30
imranstein <imranabdul22@gmail.com>
2023-02-26 12:26:10 +0300

    Update footer.blade.php

--- resources/views/Front/layouts/footer.blade.php
+++ resources/views/Front/layouts/footer.blade.php
@@ -23,7 +23,7 @@
                         Copyright &copy;<script>
                             document.write(new Date().getFullYear());
 
-                        </script> All rights reserved | This website is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://greentechet.com" target="_blank">Greentech</a>
+                        </script> All rights reserved | This website is developed by <a href="https://greentechet.com" target="_blank">Greentech</a>
 
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </p>
